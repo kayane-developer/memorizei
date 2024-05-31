90a8fb40a37a14dd35fb11ac4fb1b1c35146dffe
Kayane Costa <730kayanecosta@gmail.com>
2024-05-31 20:20:38 -0300

    Criação do projeto java e spring boot

---
+++ src/main/java/com/memorizei/MemorizeiApplication.java
@@ -0,0 +1,13 @@
+package com.memorizei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorizeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorizeiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/memorizei/MemorizeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memorizei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
