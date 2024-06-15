32042eeaa089da280178e4e6352c8d0270d5e082
Kayane Costa <730kayanecosta@gmail.com>
2024-06-15 00:43:08 -0300

    /

--- src/main/java/com/memorizei/exception/RestExceptionHandler.java
+++ src/main/java/com/memorizei/exception/RestExceptionHandler.java
@@ -1,13 +1,13 @@
 package com.memorizei.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
